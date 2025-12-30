2d502317fd865a85de7ba8befe9042dc0416bf54
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2025-12-30 17:53:43 +0530

    Added elements

---
+++ Scenes/sprite_2d.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+
+
+func _process(delta: float) -> void:
+	rotation += 5*delta
--- Scripts/Kill Zone.gd
+++ Scripts/Kill Zone.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 
-
-
 func _on_spike_1_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
@@ -13,6 +11,14 @@ func _on_lava_body_entered(body: CharacterBody2D) -> void:
 	print(str(Global.health))
 
 
-func _on_spike_2_body_entered(body: Node2D) -> void:
+func _on_spike_2_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
+	print(str(Global.health))
+	
+func _on_spike_3_body_entered(body: CharacterBody2D) -> void:
+	Global.health -= 1
+	print(str(Global.health))
+
+func _on_rotating_spike_body_entered(body: CharacterBody2D) -> void:
+	Global.health -= 2
 	print(str(Global.health))
