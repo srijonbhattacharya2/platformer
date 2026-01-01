e837613bc431d61500fdd6c9ba460014e919f036
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2026-01-01 16:23:27 +0530

    Cimmit

--- Scripts/Kill Zone.gd
+++ Scripts/Kill Zone.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+
+
 func _on_spike_1_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
@@ -11,9 +13,10 @@ func _on_lava_body_entered(body: CharacterBody2D) -> void:
 	print(str(Global.health))
 
 
-func _on_spike_2_body_entered(body: CharacterBody2D) -> void:
+func _on_spike_2_body_entered(body: Node2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
+<<<<<<< HEAD
 	
 func _on_spike_3_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
@@ -50,3 +53,5 @@ func _on_spike_5_body_entered(body: CharacterBody2D) -> void:
 func _on_spike_6_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
+=======
+>>>>>>> parent of 2d50231 (Added elements)
