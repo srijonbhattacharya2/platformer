c3997a629e01b84cb7b744fd37aeaf1178712182
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2026-01-01 16:18:53 +0530

    Revert "Added Double Jump"
    This reverts commit 0a8450534e16c7ed889a13e03b281294ed734f05.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 700.0
-const JUMP_VELOCITY = -800.0
-const MAX_JUMPS = 2
 
-var jump_count := 0
+const SPEED = 700.0
+const JUMP_VELOCITY = -1000.0
+const GRAVITY = -700
+
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	else:
-		jump_count = 0  
 
-
-	if Input.is_action_just_pressed("ui_up") and jump_count < MAX_JUMPS:
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		jump_count += 1
 
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
