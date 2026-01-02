b8106f9ce0275d4fcee7bcc76bfc657bb10858a6
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2026-01-02 14:18:27 +0530

    Edited Level

---
+++ Scenes/collision_shape_2d.gd
@@ -0,0 +1,5 @@
+extends CollisionShape2D
+
+
+func _process(delta: float) -> void:
+	rotation += 1*delta
--- Scripts/Kill Zone.gd
+++ Scripts/Kill Zone.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
 
-
-
 func _on_spike_1_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
@@ -51,4 +49,9 @@ func _on_spike_5_body_entered(body: CharacterBody2D) -> void:
 
 func _on_spike_6_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
+	print(str(Global.health))
+
+
+func _on_lava_2_body_entered(body: CharacterBody2D) -> void:
+	Global.health = 0
 	print(str(Global.health))
