2c6fd4689f2d3b59e50eccfce950b0813dc39293
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2026-01-04 15:34:39 +0530

    level changes

--- Scripts/Kill Zone.gd
+++ Scripts/Kill Zone.gd
@@ -32,12 +32,6 @@ func _on_spike_4_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
 
-
-func _on_empty_area_body_entered(body: CharacterBody2D) -> void:
-	Global.health = 0
-	print(str(Global.health))
-
-
 func _on_rotating_spike_3_body_entered(body: CharacterBody2D) -> void:
 	Global.health -= 1
 	print(str(Global.health))
