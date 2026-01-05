da9a290bdd5895647c464351c32a6e59eb08048f
Srijon Bhattacharya <srijon.bhattacharya2@gmail.com>
2026-01-05 17:17:45 +0530

    Level Changes (some broken)

---
+++ Scenes/platform.gd
@@ -0,0 +1,15 @@
+extends StaticBody2D
+
+var SPEED = 10
+var dir = -1
+
+func _process(delta: float) -> void:
+	position.x += SPEED * dir
+
+
+func _on_area_2d_body_entered(body: Area2D) -> void:
+	dir = 1
+
+
+func _on_area_2d_2_body_entered(body: Area2D) -> void:
+	dir = -1
